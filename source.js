eef21324021887337ee518fda22571f819169212
Paula Carril Torrens <PAULAANDREACARRIL@GMAIL.COM>
2021-09-07 17:54:25 +1000

    including real data in our forecast, format day of forecast, mathround the forecast temps,

--- source.js
+++ source.js
@@ -27,24 +27,42 @@ function displayTemp(response) {
   getForecast(response.data.coord);
 }
 
-function displayForecast() {
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
+
+function displayForecast(response) {
+  console.log(response.data.daily);
+  let forecast = response.data.daily;
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
-  let days = ["Thu", "Fri", "Sat", "Sun", "Mon", "Tue"];
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `<div class="col-2">
-        <div class="weather-forecast-day">${day}</div>
+
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `<div class="col-2">
+        <div class="weather-forecast-day">${formatDay(forecastDay.dt)}</div>
              <img
-             src="https://openweathermap.org/img/wn/09d@2x.png"
+             src="https://openweathermap.org/img/wn/${
+               forecastDay.weather[0].icon
+             }@2x.png"
              alt=""
              width="40px"/>
         <div class="weather-forecast-temp">
-        <span class="weather-forecast-temp-max">18 </span> / <span class="weather-forecast-temp-min">12</span>
+        <span class="weather-forecast-temp-max">${Math.round(
+          forecastDay.temp.max
+        )} </span> / <span class="weather-forecast-temp-min">${Math.round(
+          forecastDay.temp.min
+        )}</span>
         </div>
     </div>`;
+    }
   });
 
   forecastHTML = forecastHTML + `</div>`;
@@ -148,5 +166,3 @@ if (minutes < 10) {
 }
 
 time.innerHTML = `${hours}:${minutes}`;
-
-searchCity("Preston");
